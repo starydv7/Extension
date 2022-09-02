2a48bfe6b04f4405a5a9264c654f96230dbf9295
Rasheedsheikh <100085425+Rasheedsheikh@users.noreply.github.com>
2022-09-02 21:07:45 +0530

    go

--- Extensions.js
+++ Extensions.js
@@ -1,7 +1,7 @@
-const generateSTYLES = () => {
+const styles = () => {
   return `<style>@import url(https://fonts.googleapis.com/css?family=opensans:500);
   body {
-    background: #33cc99;
+ background: black;
     color:#0f7dae ;
     font-family: "Open Sans", sans-serif;
     max-height: 700px;
@@ -15,18 +15,18 @@ const generateSTYLES = () => {
     margin: 100px auto;
   }
   ._404 {
-    font-size: 220px;
+    font-size: 50px;
     position: relative;
     display: inline-block;
     z-index: 2;
-    height: 180px;
-    letter-spacing: 18px;
+    height: 100px;
+    letter-spacing: 2px;
   }
   ._1 {
     text-align: center;
     display: block;
     position: relative;
-    letter-spacing: 12px;
+    letter-spacing: 2px;
     font-size: 4em;
     line-height: 80%;
   }
@@ -55,183 +55,9 @@ const generateSTYLES = () => {
     width: 60%;
   }
   
-  hr {
-    padding: 0;
-    border: none;
-    border-top: 5px solid #fff;
-    color: #fff;
-    text-align: center;
-    margin: 0px auto;
-    width: 420px;
-    height: 10px;
-    z-index: -10;
-  }
+ 
   
-  hr:after {
-    display: inline-block;
-    position: relative;
-    top: -0.75em;
-    font-size: 2em;
-    padding: 0 0.2em;
-    background: #33cc99;
-  }
   
-  .cloud {
-    width: 350px;
-    height: 120px;
-  
-    background: #fff;
-    background: linear-gradient(top, #fff 100%);
-    background: -webkit-linear-gradient(top, #fff 100%);
-    background: -moz-linear-gradient(top, #fff 100%);
-    background: -ms-linear-gradient(top, #fff 100%);
-    background: -o-linear-gradient(top, #fff 100%);
-  
-    border-radius: 100px;
-    -webkit-border-radius: 100px;
-    -moz-border-radius: 100px;
-  
-    position: absolute;
-    margin: 120px auto 20px;
-    z-index: -1;
-    transition: ease 1s;
-  }
-  
-  .cloud:after,
-  .cloud:before {
-    content: "";
-    position: absolute;
-    background: #fff;
-    z-index: -1;
-  }
-  
-  .cloud:after {
-    width: 100px;
-    height: 100px;
-    top: -50px;
-    left: 50px;
-  
-    border-radius: 100px;
-    -webkit-border-radius: 100px;
-    -moz-border-radius: 100px;
-  }
-  
-  .cloud:before {
-    width: 180px;
-    height: 180px;
-    top: -90px;
-    right: 50px;
-  
-    border-radius: 200px;
-    -webkit-border-radius: 200px;
-    -moz-border-radius: 200px;
-  }
-  
-  .x1 {
-    top: -50px;
-    left: 100px;
-    -webkit-transform: scale(0.3);
-    -moz-transform: scale(0.3);
-    transform: scale(0.3);
-    opacity: 0.9;
-    -webkit-animation: moveclouds 15s linear infinite;
-    -moz-animation: moveclouds 15s linear infinite;
-    -o-animation: moveclouds 15s linear infinite;
-  }
-  
-  .x1_5 {
-    top: -80px;
-    left: 250px;
-    -webkit-transform: scale(0.3);
-    -moz-transform: scale(0.3);
-    transform: scale(0.3);
-    -webkit-animation: moveclouds 17s linear infinite;
-    -moz-animation: moveclouds 17s linear infinite;
-    -o-animation: moveclouds 17s linear infinite;
-  }
-  
-  .x2 {
-    left: 250px;
-    top: 30px;
-    -webkit-transform: scale(0.6);
-    -moz-transform: scale(0.6);
-    transform: scale(0.6);
-    opacity: 0.6;
-    -webkit-animation: moveclouds 25s linear infinite;
-    -moz-animation: moveclouds 25s linear infinite;
-    -o-animation: moveclouds 25s linear infinite;
-  }
-  
-  .x3 {
-    left: 250px;
-    bottom: -70px;
-  
-    -webkit-transform: scale(0.6);
-    -moz-transform: scale(0.6);
-    transform: scale(0.6);
-    opacity: 0.8;
-  
-    -webkit-animation: moveclouds 25s linear infinite;
-    -moz-animation: moveclouds 25s linear infinite;
-    -o-animation: moveclouds 25s linear infinite;
-  }
-  
-  .x4 {
-    left: 470px;
-    botttom: 20px;
-  
-    -webkit-transform: scale(0.75);
-    -moz-transform: scale(0.75);
-    transform: scale(0.75);
-    opacity: 0.75;
-  
-    -webkit-animation: moveclouds 18s linear infinite;
-    -moz-animation: moveclouds 18s linear infinite;
-    -o-animation: moveclouds 18s linear infinite;
-  }
-  
-  .x5 {
-    left: 200px;
-    top: 300px;
-  
-    -webkit-transform: scale(0.5);
-    -moz-transform: scale(0.5);
-    transform: scale(0.5);
-    opacity: 0.8;
-  
-    -webkit-animation: moveclouds 20s linear infinite;
-    -moz-animation: moveclouds 20s linear infinite;
-    -o-animation: moveclouds 20s linear infinite;
-  }
-  
-  @-webkit-keyframes moveclouds {
-    0% {
-      margin-left: 1000px;
-    }
-    100% {
-      margin-left: -1000px;
-    }
-  }
-  @-moz-keyframes moveclouds {
-    0% {
-      margin-left: 1000px;
-    }
-    100% {
-      margin-left: -1000px;
-    }
-  }
-  @-o-keyframes moveclouds {
-    0% {
-      margin-left: 1000px;
-    }
-    100% {
-      margin-left: -1000px;
-    }
-  }
-  .main{
-    width:200px;
-    height:200px;
-  }
    </style>`;
 };
 
@@ -239,53 +65,54 @@ const generateHTML = (pageName) => {
   return `
    
    <div id="clouds">
-      <div class="cloud x1"></div>
-      <div class="cloud x1_5"></div>
-      <div class="cloud x2"></div>
-      <div class="cloud x3"></div>
-      <div class="cloud x4"></div>
-      <div class="cloud x5"></div>
+      <div ></div>
+      <div></div>
+      <div ></div>
+      <div ></div>
+      <div ></div>
+      <div ></div>
   </div>
   <div class='c'>
-      <div class='_404'>404</div>
-      <hr>
-      <div class='_1'>GET BACK TO WORK</div>
-      <div class='_2'>STUDYING > ${pageName}</div>
+      <div class='_404'>404 source not found</div>
+    
+      <div class='_1'>Page has been blocked</div>
+      <div class='_2'>Unblock ${pageName} to access</div>
   </div>
    `;
 };
 
-switch (window.location.hostname) {
+switch (window.location.hostname) 
+{
   case "www.youtube.com":
-    document.head.innerHTML = generateSTYLES();
+    document.head.innerHTML = styles();
     document.body.innerHTML = generateHTML("YOUTUBE");
     break;
   case "www.facebook.com":
-    document.head.innerHTML = generateSTYLES();
+    document.head.innerHTML = styles();
     document.body.innerHTML = generateHTML("FACEBOOK");
     break;
   case "www.netflix.com":
-    document.head.innerHTML = generateSTYLES();
+    document.head.innerHTML = styles();
     document.body.innerHTML = generateHTML("NETFLIX");
     break;
   case "discord.com":
-    document.head.innerHTML = generateSTYLES();
+    document.head.innerHTML = styles();
     document.body.innerHTML = generateHTML("DISCORD");
     break;
   case "www.spotify.com":
-    document.head.innerHTML = generateSTYLES();
+    document.head.innerHTML = styles();
     document.body.innerHTML = generateHTML("SPOTIFY");
     break;
-    case "www.masaischool.com":
-    document.head.innerHTML = generateSTYLES();
-    document.body.innerHTML = generateHTML("MASAI");
+    case "www.reddit.com":
+    document.head.innerHTML = styles();
+    document.body.innerHTML = generateHTML("Reddit");
     break;
   case "www.linkedin.com":
-    document.head.innerHTML = generateSTYLES();
+    document.head.innerHTML = styles();
     document.body.innerHTML = generateHTML("LINKEDIN");
     break;
    case "www.github.com":
-    document.head.innerHTML = generateSTYLES();
+    document.head.innerHTML = styles();
     document.body.innerHTML = generateHTML("GITHUB");
     break;
 }
