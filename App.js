d2331b7cdf3290aec2839ebf9cca8b0c406a6540
Rasheedsheikh <100085425+Rasheedsheikh@users.noreply.github.com>
2022-09-03 09:38:27 +0530

    go

--- App.js
+++
@@ -1,9 +0,0 @@
-var second = 0;
-function max(val) {
-  return val > 9 ? val : "0" + val;
-}
-setInterval(function ()
- {
-  document.getElementById("seconds").innerHTML = max(++second % 60);
-  document.getElementById("minutes").innerHTML = max(parseInt(second / 60, 10));
-}, 1000);
